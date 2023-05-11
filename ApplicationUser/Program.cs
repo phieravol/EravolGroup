ed60a38ff4174b6048815d969612f02175c128b5
phinqevol <phinqevol@gmail.com>
2023-05-11 16:45:42 +0700

    p231/f3-AuthorHead: Add Swagger Author header

--- ApplicationUser/Program.cs
+++ ApplicationUser/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Security.Cryptography.Xml;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +14,37 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+	config.SwaggerDoc("v1", new OpenApiInfo { Title = "Swagger EravolGroup Solution", Version = "v1" });
+	config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Description = "@JWT Authorization header using the Bearer schema. \r\n\r\n" +
+			"Enter 'Bearer' [Space] and then your token in the text input below. \r\n\r\n" +
+			"Example: 123456abcdef",
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer"
+	});
+	config.AddSecurityRequirement(new OpenApiSecurityRequirement()
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				},
+				Scheme = "oauth2",
+				Name = "Bearer",
+				In = ParameterLocation.Header,
+			},
+			new List<string>()
+		}
+	});
+});
 
 // Register and Config Identity
 builder.Services.AddIdentity<AppUser, IdentityRole<Guid>>()
