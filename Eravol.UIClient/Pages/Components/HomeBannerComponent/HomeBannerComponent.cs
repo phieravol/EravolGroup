ae60eb11c080f62accd401d3ca0f7b0c3e3e11b1
phinqevol <phinqevol@gmail.com>
2023-05-14 23:03:02 +0700

    p231/f5-UserProfile: Add Frontend web client

---
+++ Eravol.UIClient/Pages/Components/HomeBannerComponent/HomeBannerComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eravol.UIClient.Pages.Components.HomeBannerComponent
+{
+	public class HomeBannerComponent : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			string Title = "Home Page";
+			return View("HomeBannerComponent", Title);
+		}
+	}
+}
