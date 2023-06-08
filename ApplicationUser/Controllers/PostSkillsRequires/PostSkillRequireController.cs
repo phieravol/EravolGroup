2650c2623dc08f10ef196ca21c24d051e1c8d612
phinqevol <phinqevol@gmail.com>
2023-06-08 13:47:06 +0700

    p231/f10-Posts: Delete Post in clients, list posts & post detail in public

--- ApplicationUser/Controllers/PostSkillsRequires/PostSkillRequireController.cs
+++ ApplicationUser/Controllers/PostSkillsRequires/PostSkillRequireController.cs
@@ -12,13 +12,22 @@ namespace Eravol.WebApi.Controllers.PostSkills
 
     public class PostSkillRequireController : ControllerBase
     {
+        #region DI Services
         private readonly IPostSkillsRepository skillRequireRepository;
+        #endregion
 
+        #region Constructor
         public PostSkillRequireController(IPostSkillsRepository skillRequireRepository)
         {
             this.skillRequireRepository = skillRequireRepository;
         }
+        #endregion
 
+        /// <summary>
+        /// Create PostSkillRequires by List of SkillRequires from Ajax
+        /// </summary>
+        /// <param name="skillRequires"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> CreatePostSkillRequires(List<CreateSkillRequiresRequest>? skillRequires)
         {
@@ -41,6 +50,38 @@ namespace Eravol.WebApi.Controllers.PostSkills
             await skillRequireRepository.CreateSkillsRequireAsync(postSkills);
 
             return Ok(skillRequires);
+        }
+
+        /// <summary>
+        /// Update Post Skill Require by Ajax
+        /// </summary>
+        /// <param name="skillRequireId"></param>
+        /// <param name="skillRequires"></param>
+        /// <returns></returns>
+        [HttpPut("{skillRequireId}")]
+        public async Task<IActionResult> UpdatePostSkillRequire(int? skillRequireId, CreateSkillRequiresRequest skillRequire)
+        {
+            //if PostSkill Require is null then return error message
+            if (skillRequireId==null)
+            {
+                return BadRequest("Id of Post Skill Require is empty");
+            }
+
+            //get old PostSkillRequire from Db
+            PostSkillRequired postSkillRequired = await skillRequireRepository.GetSkillRequireById(skillRequireId);
+
+            //if postSkillRequired not found then return error message
+            if (postSkillRequired == null)
+            {
+                return NotFound("PostSkillRequire not found");
+            }
+
+            //set new value for old PostSkillRequired
+            postSkillRequired.SkillId = skillRequire.SkillId;
+
+            //process update PostSkillRequire
+            await skillRequireRepository.UpdateSkillRequire(postSkillRequired);
+            return Ok(skillRequire);
         }
 
     }
---
+++ ApplicationUser/Repositories/Posts/Public/PostsPublicRepository.cs
@@ -0,0 +1,107 @@
+﻿using Eravol.UserWebApi.Data;
+using Eravol.WebApi.Data.Models;
+using Eravol.WebApi.ViewModels.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
+namespace Eravol.WebApi.Repositories.Posts.Public
+{
+    public class PostsPublicRepository : IPostsPublicRepository
+    {
+        private readonly EravolUserWebApiContext context;
+
+        public PostsPublicRepository(EravolUserWebApiContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Post>> GetPostSearchPaging(PagingRequestBase<Post> request)
+        {
+            List<Post> posts = new List<Post>();
+            try
+            {
+                var query = from p in context.Posts
+                            join c in context.Categories on p.CategoryId equals c.CategoryId into subCatejoined
+                            from cateJoined in subCatejoined.DefaultIfEmpty()
+                            join u in context.AppUsers on p.UserId equals u.Id
+                            select new { p, cateJoined, u };
+
+                if (!string.IsNullOrEmpty(request.SearchTerm))
+                {
+                    query = query.Where(x => x.p.PostTitle.Contains(request.SearchTerm) ||
+                            x.p.SortDesc.Contains(request.SearchTerm) ||
+                            x.p.PostDetails.Contains(request.SearchTerm));
+                }
+
+                posts = await query.Select(x => new Post()
+                {
+                    PostId = x.p.PostId,
+                    PostTitle = x.p.PostTitle,
+                    SortDesc = x.p.SortDesc,
+                    PostDetails = x.p.PostDetails,
+                    UserId = x.u.Id,
+                    AppUser = x.u,
+                    Budget = x.p.Budget,
+                    Categories = x.cateJoined,
+                    CategoryId = x.p.CategoryId,
+                    ExpirationDate = x.p.ExpirationDate,
+                    LevelRequired = x.p.LevelRequired,
+                    PostedDate = x.p.PostedDate,
+                    PostSkillRequired = x.p.PostSkillRequired,
+                    PostStatus = x.p.PostStatus,
+                    PostStatusId = x.p.PostStatusId,
+                }).ToListAsync();
+
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return posts;
+        }
+
+		public async Task<Post?> GetPublicPostById(int? postId)
+        {
+            Post? post = new Post();
+			try
+			{
+				var query = from p in context.Posts
+							join c in context.Categories on p.CategoryId equals c.CategoryId into subCatejoined
+							from cateJoined in subCatejoined.DefaultIfEmpty()
+							join u in context.AppUsers on p.UserId equals u.Id
+							join ps in context.PostStatuses on p.PostStatusId equals ps.PostStatusId into subStatus
+							from statusJoined in subStatus.DefaultIfEmpty()
+							where p.PostId == postId
+							select new { p, cateJoined, u, statusJoined };
+
+				
+				post = await query.Select(x => new Post()
+				{
+					PostId = x.p.PostId,
+					PostTitle = x.p.PostTitle,
+					SortDesc = x.p.SortDesc,
+					PostDetails = x.p.PostDetails,
+					UserId = x.p.UserId,
+					AppUser = x.u,
+					Budget = x.p.Budget,
+					Categories = x.cateJoined,
+					CategoryId = x.p.CategoryId,
+					ExpirationDate = x.p.ExpirationDate,
+					LevelRequired = x.p.LevelRequired,
+					PostedDate = x.p.PostedDate,
+					PostSkillRequired = x.p.PostSkillRequired,
+					PostStatus = x.p.PostStatus,
+					PostStatusId = x.p.PostStatusId,
+				}).FirstOrDefaultAsync();
+
+			}
+			catch (Exception e)
+            {
+				throw new Exception(e.Message);
+			}
+			return post;
+			
+
+		}
+	}
+}
