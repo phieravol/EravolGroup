92f57e36770359982afb751eeb5ae8fcd3066b65
phieravol <121568897+phieravol@users.noreply.github.com>
2023-06-28 17:40:58 +0700

    p231/f11-FixUX: Logout & update header base on user role

--- Eravol.UIClient/Pages/User/Logout.cshtml.cs
+++ Eravol.UIClient/Pages/User/Logout.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NuGet.Common;
 
 namespace Eravol.UIClient.Pages.User
 {
@@ -9,7 +10,16 @@ namespace Eravol.UIClient.Pages.User
     {
 		public async Task<IActionResult> OnGetAsync()
 		{
-			await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+			HttpContext.Session.Clear();
+			HttpContext.Session.Remove("AuthToken");
+			HttpContext.Session.Remove("UserId");
+			HttpContext.Session.Remove("LoginStatus");
+			HttpContext.Session.Remove("Username");
+			HttpContext.Session.Remove("Fullname");
+			HttpContext.Session.Remove("Email");
+			HttpContext.Session.Remove("PhoneNumber");
+			HttpContext.Session.Remove("Roles");
+			await HttpContext.Session.CommitAsync();
 			return RedirectToPage("/Index");
 		}
 	}
