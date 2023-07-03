a1042f8d65a9f09ee8a580348118b00b7c9b2389
phieravol <121568897+phieravol@users.noreply.github.com>
2023-07-03 18:21:45 +0700

    p231/b2-FixPosts: Public paging post (progressing)

---
+++ ApplicationUser/ViewModels/Posts/Public/PostPublicViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Eravol.WebApi.ViewModels.Posts.Public
+{
+	public class PostPublicViewModel
+	{
+		public int PostId { get; set; }
+		public string PostTitle { get; set; }
+		public string? SortDesc { get; set; }
+		public string PostDetails { get; set; }
+		public decimal Budget { get; set; }
+		public DateTime ExpirationDate { get; set; }
+		public DateTime PostedDate { get; set; }
+		public DateTime? LastUpdatedDate { get; set; }
+		public int CategoryId { get; set; }
+		public Guid UserId { get; set; }
+		public int PostStatusId { get; set; }
+		public string LevelRequired { get; set; }
+	}
+}
---
+++ Eravol.UIClient/Repositories/Categories/PublicCategoryService.cs
@@ -0,0 +1,39 @@
+﻿using Eravol.WebApi.Data.Models;
+using Newtonsoft.Json;
+using NuGet.Common;
+using System.Net.Http.Headers;
+
+namespace Eravol.UIClient.Repositories.Categories
+{
+    public class PublicCategoryService : IPublicCategoryService
+    {
+        #region Constants
+        private const string BASE_URL = "https://localhost:7259";
+        private const string CATEGORY_PUBLIC_URL = "api/PublicCategories";
+        #endregion
+
+        #region Dependencies injection services
+        private readonly IHttpClientFactory clientFactory;
+        #endregion
+
+        #region Constructor
+        public PublicCategoryService(IHttpClientFactory clientFactory)
+        {
+            this.clientFactory = clientFactory;
+        }
+        #endregion
+
+        public async Task<List<Category>> GetAllPublicCategory()
+        {
+            //Create new client to send request to api
+            var client = clientFactory.CreateClient();
+            client.BaseAddress = new Uri(BASE_URL);
+
+            //Get response from api
+            HttpResponseMessage response = await client.GetAsync(CATEGORY_PUBLIC_URL);
+            string dataResponse = await response.Content.ReadAsStringAsync();
+            List<Category>? categories = JsonConvert.DeserializeObject<List<Category>>(dataResponse);
+            return categories;
+        }
+    }
+}
